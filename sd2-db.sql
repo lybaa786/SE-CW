2abf1482995857232293ed1b3cf8b6bc706a0657
Tobiobafemi <obafemitobi07@gmail.com>
2026-03-24 17:18:54 +0000

    Edited database name and content

--- sd2-db.sql
+++ sd2-db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Oct 30, 2022 at 09:54 AM
--- Server version: 8.0.24
--- PHP Version: 7.4.20
+-- Generation Time: Mar 24, 2026 at 04:51 PM
+-- Server version: 9.6.0
+-- PHP Version: 8.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,45 +20,143 @@ SET time_zone = "+00:00";
 --
 -- Database: `sd2-db`
 --
+CREATE DATABASE IF NOT EXISTS `sd2-db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `sd2-db`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_table`
+-- Table structure for table `Account`
 --
 
-CREATE TABLE `test_table` (
+CREATE TABLE `Account` (
   `id` int NOT NULL,
-  `name` varchar(512) NOT NULL
+  `name` varchar(250) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist_tags`
+--
+
+CREATE TABLE `playlist_tags` (
+  `playlist_id` int NOT NULL,
+  `tag_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `test_table`
+-- Dumping data for table `tags`
 --
 
-INSERT INTO `test_table` (`id`, `name`) VALUES
-(1, 'Lisa'),
-(2, 'Kimia');
+INSERT INTO `tags` (`id`, `name`) VALUES
+(10, '80s'),
+(11, 'Accoustic'),
+(9, 'Afro'),
+(18, 'Blues'),
+(13, 'Classical'),
+(16, 'Dance'),
+(2, 'Drill'),
+(4, 'Gospel'),
+(8, 'Hip-Hop'),
+(6, 'jazz'),
+(15, 'K-Pop'),
+(17, 'Party'),
+(5, 'Pop and Rock'),
+(7, 'R&B'),
+(3, 'Rap'),
+(12, 'Reggae'),
+(14, 'Soul/Funk'),
+(1, 'Worship');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `test_table`
+-- Indexes for table `Account`
 --
-ALTER TABLE `test_table`
+ALTER TABLE `Account`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `playlist_tags`
+--
+ALTER TABLE `playlist_tags`
+  ADD PRIMARY KEY (`playlist_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `test_table`
+-- AUTO_INCREMENT for table `Account`
 --
-ALTER TABLE `test_table`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `Account`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `playlist_tags`
+--
+ALTER TABLE `playlist_tags`
+  ADD CONSTRAINT `playlist_tags_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `playlist_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
