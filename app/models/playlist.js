aa221b2a8d853bfcb8443222f0f2756c5865ebea
lybaa786 <lybaa611@gmail.com>
2026-04-23 12:50:16 +0100

    Add vibe rating form to playlist detail page

--- app/models/playlist.js
+++ app/models/playlist.js
@@ -1,43 +1,30 @@
-//Get the functions in the db.js file to use
-
 const db = require('../services/db');
 
 class Playlist {
-    //playlist ID
-    
     id;
-
-    //Playlist name
-
     name;
-
-    //playlist description
-
     decription;
-
-    //playlist songs 
-
+    user_id;
+    owner_name;
     songs = [];
-
-    //playlist tags
-    
     tags = [];
 
     constructor(id) {
         this.id = id;
     }
 
-
     async getPlaylistName() {
-
         if (typeof this.name !== 'string') {
-
-            const sql = "SELECT title, description FROM playlist WHERE id = ?";
+            const sql = `SELECT p.title, p.description, p.user_id, u.name as owner_name
+                         FROM playlist p
+                         LEFT JOIN users u ON u.id = p.user_id
+                         WHERE p.id = ?`;
             const results = await db.query(sql, [this.id]);
-
             if (results.length > 0) {
                 this.name = results[0].title;
                 this.decription = results[0].description;
+                this.user_id = results[0].user_id;
+                this.owner_name = results[0].owner_name;
             }
         }
     }
@@ -58,34 +45,28 @@ class Playlist {
         return this.tags;
     }
 
-    async  updatePlaylistName(title, description) {
-
+    async updatePlaylistName(title, description) {
         const sql = "UPDATE playlist SET title = ?, description = ? WHERE id = ?";
         await db.query(sql, [title, description, this.id]);
-
         this.name = title;
         this.decription = description;
     }
 
     async deletePlaylist() {
-
-
-        const sql = "DELETE FROM  playlist WHERE id =?";
+        const sql = "DELETE FROM playlist WHERE id = ?";
         await db.query(sql, [this.id]);
     }
 
     static async createPlaylist(title, description) {
-
         const sql = "INSERT INTO playlist (title, description) VALUES (?, ?)";
         const result = await db.query(sql, [title, description]);
         return new Playlist(result.insertId);
     }
 
     static async getAll() {
-
-     const sql = "SELECT * FROM playlist";
-     return await db.query(sql);
-}
+        const sql = "SELECT * FROM playlist";
+        return await db.query(sql);
+    }
 }
 
 module.exports = Playlist;
